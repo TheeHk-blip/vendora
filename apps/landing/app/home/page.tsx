84f522efc2804b9168078a766b0cae5ec9857a37
Peter Muguro <muguropeter15@gmail.com>
2025-11-24 22:23:17 +0300

    add dynamic export to enable server-side rendering

--- apps/landing/app/home/page.tsx
+++ apps/landing/app/home/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Metadata } from "next";
 import BuiltForEveryone from "../components/builtforall";
 import FeaturesSection from "../components/features";
