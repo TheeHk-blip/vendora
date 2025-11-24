c36345b13a4a425252d288c24b85d07e362e0ace
Peter Muguro <muguropeter15@gmail.com>
2025-11-24 15:53:29 +0300

    fix: prerender error

--- apps/landing/app/page.tsx
+++ apps/landing/app/page.tsx
@@ -3,7 +3,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 
-export default async function Index() {
+export async function Index() {
   const session = await getServerSession(authOptions);
 
   if (!session?.user.role) {
