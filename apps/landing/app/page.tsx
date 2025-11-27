6f8271686a56809323b72f7363e3cce31c6a3f28
Peter Muguro <muguropeter15@gmail.com>
2025-11-27 09:47:14 +0300

    refactor: removed all home components to figure whcich is calling auth

--- apps/landing/app/page.tsx
+++ apps/landing/app/page.tsx
@@ -1,9 +1,4 @@
 import { Metadata } from "next";
-import BuiltForEveryone from "./components/builtforall";
-import FeaturesSection from "./components/features";
-import Footer from "./components/footer";
-import Hero from "./components/hero";
-import HowVendoraWorks from "./components/howvendoraworks";
 
 export const metadata: Metadata = {
   title: "Home | Vendora",
@@ -13,11 +8,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return(
     <div className="flex flex-col justify-center px-2 py-2 w-full max-w-7xl" >
-      <Hero />
-      <BuiltForEveryone />
-      <HowVendoraWorks />
-      <FeaturesSection />
-      <Footer />
+      <h1 className="text-3xl font-bold">Welcome to Vendora</h1>
     </div>
   )
 }
