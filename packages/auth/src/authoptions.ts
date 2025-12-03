bc2dd0f73f9275d09b230640f5fb61e8ffead688
Peter Muguro <muguropeter15@gmail.com>
2025-12-03 10:43:14 +0300

    feat: added cookies for session tracking

--- packages/auth/src/authoptions.ts
+++ packages/auth/src/authoptions.ts
@@ -40,7 +40,7 @@ export const authOptions: NextAuthOptions = {
     })
   ],
 
- /* cookies: {
+  cookies: {
     sessionToken: {
       name:"_Secure-next-auth.session.token",
       options: {
@@ -52,7 +52,7 @@ export const authOptions: NextAuthOptions = {
       }      
     }
   },
-*/
+  
   session: {
     strategy: "jwt",
     maxAge: 60 * 60 * 24, // 1 day(s)
