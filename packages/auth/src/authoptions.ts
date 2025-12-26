562e2d8aa963cb770c34fa6cf45a6ff91fdca452
Peter Muguro <muguropeter15@gmail.com>
2025-12-26 19:30:14 +0300

    fix: reactivated cookies

--- packages/auth/src/authoptions.ts
+++ packages/auth/src/authoptions.ts
@@ -43,7 +43,7 @@ export const authOptions: NextAuthOptions = {
     })
   ],
 
-/*  cookies: {
+  cookies: {
     sessionToken: {
       name:"_Secure-next-auth.session.token",
       options: {
@@ -55,7 +55,7 @@ export const authOptions: NextAuthOptions = {
       }      
     }
   },
-*/
+
   session: {
     strategy: "jwt",
     maxAge: 60 * 60 * 24, // 1 day(s)
