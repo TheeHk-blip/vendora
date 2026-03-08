16eaa5272152b944ae234edfae34a15058c3b7bd
Peter Muguro <muguropeter15@gmail.com>
2026-03-08 14:15:47 +0300

    fix: variable rename

--- apps/store/app/store/checkout/page.tsx
+++ apps/store/app/store/checkout/page.tsx
@@ -98,7 +98,7 @@ export default function Checkout() {
         checkoutSelection
       };
 
-      const response = await fetch(`${process.env.SERVER}/payments${endpoint}`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/payments${endpoint}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
