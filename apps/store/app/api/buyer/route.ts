8edc43694373b8b7ef748d69d57a60bb408319b4
Peter Muguro <muguropeter15@gmail.com>
2026-05-31 15:33:42 +0300

    fix: force-dynamic cannot be used when cache components are active opted for connection

--- apps/store/app/api/buyer/route.ts
+++ apps/store/app/api/buyer/route.ts
@@ -1,9 +1,8 @@
 import { Buyer, connectDB } from "@vendora/db/frontend";
-import { NextRequest, NextResponse } from "next/server";
-
-export const dynamic = "force-dynamic";
+import { connection, NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  await connection();
   await connectDB();
   const { searchParams } = new URL(req.url);
   const id = searchParams.get("id");
